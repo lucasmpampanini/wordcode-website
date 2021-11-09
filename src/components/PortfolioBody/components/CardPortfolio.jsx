1b28852c90fca4fbc8671065f159a8f1bc202459
Lucas Pampanini <lmp.pampanini@gmail.com>
2021-11-09 11:25:07 -0300

    final

--- src/components/PortfolioBody/components/CardPortfolio.jsx
+++ src/components/PortfolioBody/components/CardPortfolio.jsx
@@ -5,7 +5,7 @@ export default function CardPortfolio({ srcImg, href }) {
   return (
     <div className='col-lg-4'>
       <a href={href} target="_blank">
-        <Image className="rounded m-2 shadow-lg" width={480} height={245} src={srcImg} />
+        <Image className="rounded m-2 shadow" width={480} height={245} src={srcImg} />
       </a> 
     </div>
   );
